f98d4ec9132c7345d1535e0285835dee3a19b745
Scott Sawyer <scott@10flow.com>
2017-05-03 21:18:37 -0400

    Fixed some bugs in the unit tests (thanks reddit\!)

--- paperlesscad/test.py
+++ paperlesscad/test.py
@@ -13,7 +13,7 @@ class TestSolution(unittest.TestCase):
     def test_chamfer(self):
         result = dfm_check('step_files/chamfer.STEP')
         issue = {'issue': 'chamfer', 'faces': None}
-        self.assertGreater(result['issues'], 0)
+        self.assertGreater(len(result['issues']), 0)
         self.assertEqual(
             {'issues': [issue]},
             result)
@@ -21,19 +21,19 @@ class TestSolution(unittest.TestCase):
     def test_radius(self):
         result = dfm_check('step_files/radius.STEP')
         issue = {'issue': 'radius', 'faces': None}
-        self.assertGreater(result['issues'], 0)
+        self.assertGreater(len(result['issues']), 0)
         self.assertEqual(
             {'issues': [issue]},
             result)
         result = dfm_check('step_files/radius_both_edges.STEP')
-        self.assertEqual(
-            {'issues': [issue]},
-            result)
+        self.assertGreater(result['issues'], 0)
+        for i in result['issues']:
+            self.assertEqual(issue, i)
 
     def test_nonuniform(self):
         result = dfm_check('step_files/nonuniform.STEP')
         issue = {'issue': 'non-uniform', 'faces': None}
-        self.assertGreater(result['issues'], 0)
+        self.assertGreater(len(result['issues']), 0)
         self.assertEqual(
             {'issues': [issue]},
             result)
@@ -44,7 +44,7 @@ class TestSolution(unittest.TestCase):
 
     def test_small_hole(self):
         result = dfm_check('step_files/small_hole_2.STEP')
-        self.assertGreater(result['issues'], 0)
+        self.assertGreater(len(result['issues']), 0)
         self.assertEqual(
             'small-hole',
             result['issues'][0]['issue'])
@@ -53,7 +53,7 @@ class TestSolution(unittest.TestCase):
     def test_small_cut(self):
         result = dfm_check('step_files/small_hole.STEP')
         # example: issue = {'issue': 'small-hole', 'faces': [1]}
-        self.assertGreater(result['issues'], 0)
+        self.assertGreater(len(result['issues']), 0)
         self.assertEqual(
             'small-cut',
             result['issues'][0]['issue'])
@@ -62,7 +62,7 @@ class TestSolution(unittest.TestCase):
     def test_draft(self):
         result = dfm_check('step_files/draft.STEP')
         issue = {'issue': 'draft', 'faces': None}
-        self.assertGreater(result['issues'], 0)
+        self.assertGreater(len(result['issues']), 0)
         self.assertEqual(
             {'issues': [issue]},
             result)
@@ -70,18 +70,18 @@ class TestSolution(unittest.TestCase):
     def test_countersink(self):
         result = dfm_check('step_files/counter_sinks.STEP')
         # example: issue = {'issue': 'counter-sink', 'faces': [1, 2]}
-        self.assertGreater(result['issues'], 0)
+        self.assertGreater(len(result['issues']), 0)
         self.assertEqual(
             'counter-sink',
             result['issues'][0]['issue'])
         self.assertTrue(len(result['issues'][0]['faces']) >= 2)
 
-    def test_countersink(self):
+    def test_counterbore(self):
         result = dfm_check('step_files/counter_bore.STEP')
-        # example: issue = {'issue': 'counter-sink', 'faces': [1]}
-        self.assertGreater(result['issues'], 0)
+        # example: issue = {'issue': 'counter-bore', 'faces': [1]}
+        self.assertGreater(len(result['issues']), 0)
         self.assertEqual(
-            'counter-sink',
+            'counter-bore',
             result['issues'][0]['issue'])
         self.assertTrue(len(result['issues'][0]['faces']) >= 1)
 
